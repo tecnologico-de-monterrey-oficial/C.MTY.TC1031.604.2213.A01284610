53c043486f0df5ce029a3924dc90fb58846f530a
A01284610 <A01284610@tec.mx>
2022-08-12 16:57:33 -0500

    clase02

---
+++ Actividades/Templates/Clase02.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "List.h"
+
+int sumInt(int n1, int n2, int n3){
+    return n1 + n2 + n3;
+}
+
+double sumDouble(double n1, double n2, double n3){
+    return n1 + n2 + n3;
+}
+
+float sumFloat(float n1, float n2, float n3){
+    return n1 + n2 + n3;
+}
+
+string sumString(string a, string b, string c){
+    return a + b + c;
+}
+
+template <class T>
+T sumAnything(T a, T b, T c){
+    return a+b+c;
+}
+
+int main() {
+/*
+    int a1 = 1;
+    int b1 = 2;
+    int c1 = 3;
+
+    double a2 = 1.4;
+    double b2 = 2.2;
+    double c2 = 3.4;
+
+    float a3 = 1.4;
+    float b3 = 2.5;
+    float c3 = 3.0;
+
+    string a4 = "hola";
+    string b4 = " crayola";
+    string c4 = " mi cajita de colores";
+
+    cout << "la suma de Integer a+b+c es igual a " << sumInt(a1,b1,c1) << endl;
+    cout << "la suma Anything de a+b+c es igual a " << sumAnything(a1,b1,c1) << endl;
+
+    cout << "\nla suma Double de a+b+c es igual a " << sumDouble(a2,b2,c2) << endl;
+    cout << "la suma Anything de a+b+c es igual a " << sumAnything(a2,b2,c2) << endl;
+
+    cout << "\nla suma Float de a+b+c es igual a " << sumFloat(a3,b3,c3) << endl;
+    cout << "la suma Anything de a+b+c es igual a " << sumAnything(a3,b3,c3) << endl;
+
+    cout << "\nla suma String de a+b+c es igual a " << sumString(a4,b4,c4) << endl;
+    cout << "la suma Anything de a+b+c es igual a " << sumAnything(a4,b4,c4) << endl;
+*/
+
+    List<string> list;
+    list.add("A");
+    list.add("B");
+    list.print();
+
+  return 0;
+}
---
+++ Actividades/Templates/List.h
@@ -0,0 +1,51 @@
+#ifndef List_h
+#define List_h
+
+template <class T>
+class List {
+    private:
+    vector<T> list;
+    int size;
+
+    public:
+    List();
+    List(vector<T> list);
+    void add(T a);
+    void print();
+};
+
+// Contructor por default
+template <class T>
+List<T>::List(){
+    //inicializar el tamaño de la lista
+    size = 0;
+    cout << "Constructor por default" << endl;
+}
+
+// Contructor con parametros default
+template <class T>
+List<T>::List(vector<T> list){
+    this->list = list;
+    size = list.size();
+    cout << "Constructor con parametros" << endl;
+}
+
+//agrega elemento a la lista
+template <class T>
+void List<T>::add(T a){
+    list.push_back(a);
+    size++;
+}
+
+// Imprimir lista
+// for (int i; i < size; i++) {
+//      cout << list[i] << endl;
+// }
+template <class T>
+void List<T>::print(){
+    for (auto el : list){
+        cout << el << endl;
+    }
+}
+
+#endif
