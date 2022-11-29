4da8a3174e58230d9c88386bd9ff5bb3a2d38cdd
A01284610 <A01284610@tec.mx>
2022-11-29 16:05:42 -0600

    hashimport

--- ADTs/08-Hash/funcionhash.cpp
+++ ADTs/08-Hash/funcionhash.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 using namespace std;
-
-int hashFunction(string mat){
-    int numMat = stoi(mat.substr(1,8));
-    return numMat % 99;
+ 
+int hashFunction(string matricula) {
+    try {
+        int numMatricula = stoi((matricula.substr(1,8)));
+        return numMatricula % 99;
+    } catch (invalid_argument &e) {
+        cout << e.what() << endl;
+    }
+    
+    
 }
 
-int main(){
-    cout << hashFunction("A01721732") << endl;
+int main() {
+ 
+    cout << hashFunction("Alexandria") << endl;
+    string a = "";
+    if (a == "") {
+        cout << "Es nulo" << endl;
+    } 
     return 0;
 }
